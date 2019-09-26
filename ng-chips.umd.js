21d07a53b82e236a811dc07448d464a06ff5918e
Ahmed Ragab <ahmed.ragab@me.com>
2019-09-26 07:38:14 +0200

    review

--- ng-chips.umd.js
+++ ng-chips.umd.js
@@ -13,9 +13,9 @@ var ChipsComponent = (function () {
         this.isFocused = false;
         this.selected = [];
         this.selectedChange = new core.EventEmitter();
-        this.edit = new core.EventEmitter();
+        this.editClicked = new core.EventEmitter();
         this.type = 'text';
-        this.showAdd = true;
+        this.showAdd = false;
         this.duplicates = false;
         this.chip = {name:''};
         this.propagateChange = function (_) { };
@@ -33,7 +33,7 @@ var ChipsComponent = (function () {
      * @param {?} event
      * @return {?}
      */
-	 
+
     ChipsComponent.prototype.addOnEnter = /**
      * @param {?} event
      * @return {?}
@@ -47,7 +47,7 @@ var ChipsComponent = (function () {
             this.remove(this.selected.length - 1);
         }
         else {
-			
+
             this.chip.name = event.target.value;
         }
     };
@@ -66,20 +66,20 @@ var ChipsComponent = (function () {
         this.chip = {name:''};
         this.propagateChange(this.selected);
         this.selectedChange.emit(this.selected);
-	
+
     };
-	ChipsComponent.prototype.clear = 
+	ChipsComponent.prototype.clear =
 	 /**
-     * @param {?} 
+     * @param {?}
      * @return {?}
      */
-    function () 
+    function ()
 	{
 		var els = document.getElementsByName('chips');
 		for (var i = 0; i < els.length;i++)
 			els[i].value = "";
 		console.log('after adding',els.length);
-	}
+	};
     /**
      * @param {?} index
      * @return {?}
@@ -95,6 +95,20 @@ var ChipsComponent = (function () {
         this.selected.splice(index, 1);
         this.propagateChange(this.selected);
         this.selectedChange.emit(this.selected);
+    };
+     /**
+     * @param {?} index
+     * @return {?}
+     */
+    ChipsComponent.prototype.edit = /**
+     * @param {?} index
+     * @return {?}
+     */
+    function (index) {
+
+        if (this.selected[index].editable == false)
+            return;
+        this.editClicked.emit(this.selected);
     };
     /*
      Form Control Value Accessor
@@ -133,7 +147,7 @@ var ChipsComponent = (function () {
     ChipsComponent.decorators = [
         { type: core.Component, args: [{
                     selector: 'jaspero-chips',
-                    template: "<span class=\"chip\" [ngClass]=\"{'chip-disabled':item.removable == false}\" *ngFor=\"let item of selected; let i = index\"> {{item.name}} <span class=\"editit\" *ngIf=\"editable\" (click)=\"edit()\"><span class=\"editit-icon\"></span></span>  <span class=\"closeit\" *ngIf=\"removable\" (click)=\"remove(i)\"><span class=\"closeit-icon\"></span></span></span> <input class=\"chip-input\" name=\"chips\" [type]=\"type\" [value]=\"chip.name\" (keyup)=\"addOnEnter($event)\" #inp (focus)=\"isFocused = true\" (focusout)=\"isFocused = false\"> <!--<button *ngIf=\"showAdd\" (click)=\"add()\">Add</button>-->",
+                    template: "<span class=\"chip\" [ngClass]=\"{'chip-disabled':item.removable == false && item.editable == false}\" *ngFor=\"let item of selected; let i = index\"> {{item.name}} <span class=\"editit\" *ngIf=\"editable || item.editable == true\" (click)=\"edit(i)\"><span class=\"editit-icon\"></span></span>  <span class=\"closeit\" *ngIf=\"removable || item.removable == true\" (click)=\"remove(i)\"><span class=\"closeit-icon\"></span></span></span> <input class=\"chip-input\" name=\"chips\" [type]=\"type\" [value]=\"chip.name\" (keyup)=\"addOnEnter($event)\" #inp (focus)=\"isFocused = true\" (focusout)=\"isFocused = false\"> <button *ngIf=\"showAdd\" (click)=\"add()\">Add</button>",
                     styles: ["jaspero-chips { display: block; border: 1px solid #e9e9e9; border-radius: 10px; padding: 5px 10px; } jaspero-chips .chip-input { height: 32px; outline: none; padding: 0; display: inline-block; border: none; background: none; transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1); transition-property: font-size; color: rgba(0, 0, 0, 0.54); font-family: inherit; line-height: 32px; width: 128px; font-size: 16px; } .chip-disabled{ opacity:.5;} jaspero-chips .chip-disabled:hover{cursor: auto !important;} .chip { height: 32px; padding: 0 15px; display: inline-block; border-radius: 32px; transition: all 0.4s cubic-bezier(0.25, 0.8, 0.25, 1); font-size: 13px; line-height: 32px; white-space: nowrap; position: relative; margin-right: 8px; margin-bottom: 4px; background-color: #e9e9e9; color: #0093c1; user-select: none; -webkit-user-select: none; cursor: pointer; overflow:hidden} jaspero-chips .chip:hover { background: #e9e9e9; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24); } /*button { position: absolute; background: #bcc987; right: 0; border: none; top: 0; height: 100%; color: #212529; cursor: pointer; }*/  .chip .closeit {font-size:14px; color:white; background-color: #0093c1; padding: 10px 7px; text-align:center; vertical-align:middle;cursor:pointer; margin-right: -15px; } .chip .editit {font-size:14px; color:white; background-color: #0093c1; padding: 10px 7px; text-align:center; vertical-align:middle;cursor:pointer; } .closeit-icon::after {content:'x'} .editit-icon::after {content:'!'}"],
                     changeDetection: core.ChangeDetectionStrategy.OnPush,
                     encapsulation: core.ViewEncapsulation.None,
@@ -151,7 +165,7 @@ var ChipsComponent = (function () {
         "inpEl": [{ type: core.ViewChild, args: ['inp',] },],
         "selected": [{ type: core.Input },],
         "selectedChange": [{ type: core.Output },],
-        "edit": [{ type: core.Output },],
+        "editClicked": [{ type: core.Output },],
         "type": [{ type: core.Input },],
         "showAdd": [{ type: core.Input },],
         "removable": [{ type: core.Input },],
